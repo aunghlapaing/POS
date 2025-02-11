96532276bd09dafb2b3020f5fd38fd9d2d0f4dad
aunghlapaing <aunghlapaing8949@gmail.com>
2025-02-11 17:24:52 +0700

    add table create

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $talbe->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('profile')->nullable();
+            $table->string('role')->nullable();
+            $table->string('provider_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
